06fcd5608c7a352e412188d3669c7dff5fed23d4
fbrio <fbrio@LAPTOP-836MB3CO>
2023-10-20 19:07:40 -0600

    Agregar archivos de proyecto.

---
+++ Projecto/resource.h
@@ -0,0 +1,48 @@
+//{{NO_DEPENDENCIES}}
+// Archivo de inclusión generado de Microsoft Visual C++.
+// Usado por Projecto.rc
+//
+#define IDC_MYICON                      2
+#define IDD_PROJECTO_DIALOG             102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define LOGIN                           103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_PROJECTO                    107
+#define IDI_SMALL                       108
+#define IDC_PROJECTO                    109
+#define IDR_MAINFRAME                   128
+#define IDR_MENU1                       129
+#define IDR_MENU2                       130
+#define VET_INFO                        131
+#define IDD_DIALOG1                     132
+#define INFODOCTOR                      132
+#define INFOEDITAR                      133
+#define IDD_DIALOG2                     134
+#define Iniciar                         1000
+#define IDC_BUTTON2                     1001
+#define IDC_EDIT1                       1002
+#define IDC_EDIT2                       1004
+#define INFO                            1008
+#define IDC_BUTTON5                     1009
+#define IDC_BUTTON6                     1010
+#define IDC_BUTTON7                     1011
+#define IDC_EDIT3                       1012
+#define IDC_EDIT4                       1013
+#define IDC_EDIT6                       1015
+#define IDC_BUTTON1                     1016
+#define IDC_HOTKEY1                     1019
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        135
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1020
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
